80f96ba6d51e52745c0bbd81d6512013c26f35e8
Dyrtull <118909194+Dyrtull@users.noreply.github.com>
2023-10-26 16:19:25 -0500

    Update views.py

--- ejercicios_practica/marvel/e_commerce/views.py
+++ ejercicios_practica/marvel/e_commerce/views.py
@@ -5,3 +5,37 @@ from django.http import JsonResponse
 def hola_mundo(request):    
     datos =  {"mensaje": "hola_mundo", "curso": "python-django"}
     return JsonResponse(datos)
+
+def get_comic_api_view(request):
+    data =     {
+        "id": 1,
+        "marvel_comic": "1010",
+        "title": "Inove",
+        "stock_qty": 6,
+        "description": "Mi primer JSON en Django",
+        "price": 10.0,
+        "picture": "https://www.django-rest-framework.org/img/logo.png"
+    }
+    return JsonResponse(data)
+
+
+    """
+    def send_json(request):
+
+    data = [{'name': 'Peter', 'email': 'peter@example.org'},
+            {'name': 'Julia', 'email': 'julia@example.org'}]
+
+    return JsonResponse(data, safe=False)
+
+
+        {
+        "id": 1,
+        "marvel_comic": "1010",
+        "title": "Inove",
+        "stock_qty": 6,
+        "description": "Mi primer JSON en Django",
+        "price": 10.0,
+        "picture": "https://www.django-rest-framework.org/img/logo.png"
+    }
+
+    """
